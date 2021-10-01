8ffbf397466afdb8b457ade1528d04477622ef25
zocker-160 <zocker1600@posteo.net>
2021-10-02 01:03:14 +0200

    remove regex from cp and mv commands

--- zabuilder.sh
+++ zabuilder.sh
@@ -5,7 +5,13 @@ makedeb -s | tee ~/$1-build.log
 
 if [ "$2" == "--container" ]
 then
-    cp ./*.deb /results/$1.deb
+    for name in ./*.deb;
+    do
+        # move the first matching file
+        mv "$name" "$1.deb"
+        cp "$1.deb" /results
+        break
+    done
 else
     cp ./*.deb /results
 fi

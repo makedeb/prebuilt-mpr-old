6bdc8d3484b4ac4cb8d1b7a8bfa81d3fa7978b31
Leo Puvilland <lpuvilla0001@mymail.lausd.net>
2021-10-02 13:47:57 -0700

    hopefully fix that weird freight issue

--- zabuilder.sh
+++ zabuilder.sh
@@ -8,8 +8,10 @@ then
     for name in ./*.deb;
     do
         # move the first matching file
+        echo "$name" > "$1.txt"
         mv "$name" "$1.deb"
         cp "$1.deb" /results
+        cp "$1.txt" /results
         break
     done
 else

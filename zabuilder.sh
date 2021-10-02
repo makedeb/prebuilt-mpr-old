b1b0d0ab933fe195e05be43a7bc3f7e0d0a0934a
zocker-160 <zocker1600@posteo.net>
2021-10-02 16:13:59 +0200

    try to fix noninteractive issue

--- zabuilder.sh
+++ zabuilder.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 echo "found $(nproc) cores"
-makedeb -s | tee ~/$1-build.log
+export DEBIAN_FRONTEND=noninteractive && makedeb -s | tee ~/$1-build.log
 
 if [ "$2" == "--container" ]
 then

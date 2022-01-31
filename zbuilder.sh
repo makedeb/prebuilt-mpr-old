02a4ae15d12742a5d4effbe0ac884823be6664ae
zocker-160 <zocker1600@posteo.net>
2022-01-31 20:04:57 +0100

    Revert "replace MPR dependency workaround with una MPR helper"
    This reverts commit d798b12941f598a409124965801887ff9091b13e.

--- zbuilder.sh
+++ zbuilder.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-codename=$(lsb_release -c -s)
-
 if test -z "$1";
 then
     echo "ERROR: pls specify a software to build!"
@@ -10,18 +8,4 @@ fi
 git clone https://mpr.makedeb.org/$1.git ~/source
 
 cd ~/source
-
-# Install all dependencies
-echo "INFO: installing all missing dependencies..."
-makedeb --print-srcinfo > SINFO
-while read -r -u 9 key value;
-do
-    if [ "$key" == "depends" ] || [ "$key" == "makedepends" ] || [ "$key" == "${codename}_depends" ] || [ "$key" == "${codename}_makedepends" ];
-    then
-        DEP=$(echo "$value" | cut -d ' ' -f2 | cut -d '>' -f1)
-        echo "MPR-helper: installing $DEP..."
-        una install "$DEP" || true
-    fi
-done 9< "SINFO"
-
 zabuilder.sh "$1" --container

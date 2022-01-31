d798b12941f598a409124965801887ff9091b13e
zocker-160 <zocker1600@posteo.net>
2022-01-31 17:45:01 +0100

    replace MPR dependency workaround with una MPR helper

--- zbuilder.sh
+++ zbuilder.sh
@@ -8,4 +8,18 @@ fi
 git clone https://mpr.makedeb.org/$1.git ~/source
 
 cd ~/source
+
+# Install all dependencies
+echo "INFO: installing all missing dependencies..."
+makedeb --print-srcinfo > SINFO
+while read -r -u 9 key value;
+do
+    if [ "$key" == "depends" ];
+    then
+        DEP=$(echo "$value" | cut -d ' ' -f2 | cut -d '>' -f1)
+        echo "installing $DEP..."
+        una install "$DEP"
+    fi
+done 9< "SINFO"
+
 zabuilder.sh "$1" --container

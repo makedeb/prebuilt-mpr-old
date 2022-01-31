92d6b69035a1f684f82484df26ee8720d10f0779
zocker-160 <zocker1600@posteo.net>
2022-01-31 18:07:20 +0100

    attempt to fix MPR dependency installation

--- zbuilder.sh
+++ zbuilder.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+codename=$(lsb_release -c -s)
+
 if test -z "$1";
 then
     echo "ERROR: pls specify a software to build!"
@@ -14,11 +16,11 @@ echo "INFO: installing all missing dependencies..."
 makedeb --print-srcinfo > SINFO
 while read -r -u 9 key value;
 do
-    if [ "$key" == "depends" ];
+    if [ "$key" == "depends" ] || [ "$key" == "makedepends" ] || [ "$key" == "${codename}_depends" ] || [ "$key" == "${codename}_makedepends" ];
     then
         DEP=$(echo "$value" | cut -d ' ' -f2 | cut -d '>' -f1)
         echo "MPR-helper: installing $DEP..."
-        una install "$DEP"
+        una install "$DEP" || true
     fi
 done 9< "SINFO"
 
